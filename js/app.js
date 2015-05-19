ba7e36c8e90edd9276980149b05c747c6b9be735
Jake Avery <jake95159@gmail.com>
2015-05-19 12:28:27 -0500

    Working banner icon hover animations using JS. Added bower installed JQuery Transit library. Next: Meet The Team section of homepage

--- js/app.js
+++ js/app.js
@@ -1,1 +1,39 @@
 $(document).foundation();
+
+/* Custom JS *********************************************/
+
+// Rise banner icons on hover
+
+// Websites icon
+$('#websites-img, #websites-shadow, .icon-link.primary').hover(function() {
+    $('#websites-img').stop().transition({y:'-5px'}, 200, 'ease');
+    $('#websites-shadow').stop().transition({opacity:1}, 200, 'ease');
+    $('.icon-link.primary').stop().transition({color:'#43D343'}, 200, 'ease');
+}, function() {
+    $('#websites-img').stop().transition({y:'5px'}, 200, 'ease');
+    $('#websites-shadow').stop().transition({opacity:0}, 200, 'ease');
+    $('.icon-link.primary').stop().transition({color:'black'}, 200, 'ease');
+});
+// Applications icon
+$('#applications-img, #applications-shadow, .icon-link.intermediate').hover(function() {
+    $('#applications-img').stop().transition({y:'-5px'}, 200, 'ease');
+    $('#applications-shadow').stop().transition({opacity:1}, 200, 'ease');
+    $('.icon-link.intermediate').stop().transition({color:'#43D3A1'}, 200, 'ease');
+}, function() {
+    $('#applications-img').stop().transition({y:'5px'}, 200, 'ease');
+    $('#applications-shadow').stop().transition({opacity:0}, 200, 'ease');
+    $('.icon-link.intermediate').stop().transition({color:'black'}, 200, 'ease');
+});
+// Solutions icon
+$('#solutions-img, #solutions-shadow, .icon-link.secondary').hover(function() {
+    $('#solutions-img').stop().transition({y:'-5px'}, 200, 'ease');
+    $('#solutions-shadow').stop().transition({opacity:1}, 200, 'ease');
+    $('.icon-link.secondary').stop().transition({color:'#43D3FF'}, 200, 'ease');
+}, function() {
+    $('#solutions-img').stop().transition({y:'5px'}, 200, 'ease');
+    $('#solutions-shadow').stop().transition({opacity:0}, 200, 'ease');
+    $('.icon-link.secondary').stop().transition({color:'black'}, 200, 'ease');
+});
+
+
+  
