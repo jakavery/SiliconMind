bf59bea6908737ef7b728533fc5c10815557696a
Jake Avery <jake95159@gmail.com>
2015-05-19 17:59:54 -0500

    Optimized JS for banner icons hover effects.

--- js/app.js
+++ js/app.js
@@ -3,41 +3,28 @@ $(document).foundation();
 /* Custom JS *********************************************/
 
 // Rise banner icons on hover
-
-// Websites icon
-$('#websites-img, #websites-shadow, .icon-link.primary').hover(function() {
-    $('#websites-img').stop().transition({y:'-5px'}, 200, 'ease');
-    $('#websites-shadow').stop().transition({opacity:1}, 200, 'ease');
-    $('.icon-link.primary').stop().transition({color:'#43D343'}, 200, 'ease');
+$('.banner-icon').find('img, .icon-link').hover(function() {
+    $(this).closest('div').find('img').not('.icon-shadow').stop().transition({y:'-5px'}, 200, 'ease');
+    $(this).closest('div').find('.icon-shadow').stop().transition({opacity:1}, 200, 'ease');
+    
+    if ($(this).closest('div').find('.icon-link').hasClass('primary')) {
+        $(this).closest('div').find('.icon-link').stop().transition({color:'#43D343'}, 200, 'ease');   
+    }
+    else if ($(this).closest('div').find('.icon-link').hasClass('intermediate')) {
+        $(this).closest('div').find('.icon-link').stop().transition({color:'#43D3A1'}, 200, 'ease');   
+    }
+    else if ($(this).closest('div').find('.icon-link').hasClass('secondary')) {
+        $(this).closest('div').find('.icon-link').stop().transition({color:'#43D3FF'}, 200, 'ease');   
+    }
+    
 }, function() {
-    $('#websites-img').stop().transition({y:'5px'}, 200, 'ease');
-    $('#websites-shadow').stop().transition({opacity:0}, 200, 'ease');
-    $('.icon-link.primary').stop().transition({color:'black'}, 200, 'ease');
-});
-// Applications icon
-$('#applications-img, #applications-shadow, .icon-link.intermediate').hover(function() {
-    $('#applications-img').stop().transition({y:'-5px'}, 200, 'ease');
-    $('#applications-shadow').stop().transition({opacity:1}, 200, 'ease');
-    $('.icon-link.intermediate').stop().transition({color:'#43D3A1'}, 200, 'ease');
-}, function() {
-    $('#applications-img').stop().transition({y:'5px'}, 200, 'ease');
-    $('#applications-shadow').stop().transition({opacity:0}, 200, 'ease');
-    $('.icon-link.intermediate').stop().transition({color:'black'}, 200, 'ease');
-});
-// Solutions icon
-$('#solutions-img, #solutions-shadow, .icon-link.secondary').hover(function() {
-    $('#solutions-img').stop().transition({y:'-5px'}, 200, 'ease');
-    $('#solutions-shadow').stop().transition({opacity:1}, 200, 'ease');
-    $('.icon-link.secondary').stop().transition({color:'#43D3FF'}, 200, 'ease');
-}, function() {
-    $('#solutions-img').stop().transition({y:'5px'}, 200, 'ease');
-    $('#solutions-shadow').stop().transition({opacity:0}, 200, 'ease');
-    $('.icon-link.secondary').stop().transition({color:'black'}, 200, 'ease');
+    $(this).closest('div').find('img').not('.icon-shadow').stop().transition({y:'5px'}, 200, 'ease');
+    $(this).closest('div').find('.icon-shadow').stop().transition({opacity:0}, 200, 'ease');
+    $(this).closest('div').find('.icon-link').stop().transition({color:'black'}, 200, 'ease');
 });
 
 
 // Team photos box-shadow on hover
-
 $('.team-photo').find('a').hover(function() {
     $(this).closest('figure').find('img').stop().transition({'box-shadow':'0 0 10px black'}, 200, 'ease');  
 }, function() {
